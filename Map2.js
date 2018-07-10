af329a3fff4bad038f3df8d0afd338eb287debb5
owner <owner@ownernoMacBook-puro.local>
2018-07-10 12:48:14 +0900

    ver 1.0.7

--- Map2.js
+++ Map2.js
@@ -1,7 +1,22 @@
 //Map.js
 var holeLayer1 = []; //穴の緯度経度の配列
+var map;
 var poly2;
 var JSTSpoly2;
+var JSTSpolyUnion;
+var x;
+// 影の緯度経度
+var kageLayer = [
+  //時計回り
+  {lat: 46.33, lng: 148.4508},
+  {lat: 24.26, lng: 148.4508},
+  {lat: 24.26, lng: 125.5911},
+  {lat: 46.33, lng: 125.5911},
+  {lat: 46.33, lng: 148.4508},
+];
+var lat25 = 0.00022457872; //緯度２５m
+var lng25 = 0.00027415956; //経度２５m
+var angle = 0.00549316406; //角度
 //var ido = 35.625993799999996;
 //var keido = 139.27856060000002;
 var googleMaps2JSTS = function(boundaries) {
@@ -22,6 +37,7 @@ var jsts2googleMaps = function(geometry) {
   return GMcoords;
 }
 
+/*
 function getPosition() {
   // 現在地を取得
   navigator.geolocation.getCurrentPosition(
@@ -52,6 +68,7 @@ function getPosition() {
   	}
   );
 }
+*/
 
 function initialize() {
   //getPosition();
@@ -61,25 +78,13 @@ function initialize() {
       var ido = position.coords.latitude;
       var keido = position.coords.longitude;
   　   //地図の表示
-      var map = new google.maps.Map(document.getElementById('map'), {
+      map = new google.maps.Map(document.getElementById('map'), {
         zoom: 20,
         center: new google.maps.LatLng(ido,keido),
       });
       var pos = map.getCenter(); //中心座標の取得
       var lat = pos.lat(); //緯度
       var lng = pos.lng(); //経度
-      var lat25 = 0.00022457872; //緯度２５m
-      var lng25 = 0.00027415956; //経度２５m
-      var angle = 0.00549316406; //角度
-      // 影の緯度経度
-      var kageLayer = [
-        //時計回り
-        {lat: 46.33, lng: 148.4508},
-        {lat: 24.26, lng: 148.4508},
-        {lat: 24.26, lng: 125.5911},
-        {lat: 46.33, lng: 125.5911},
-        {lat: 46.33, lng: 148.4508},
-      ];
       // 穴の緯度経度
       for (var i = 1; i < 65538; i++) {
         holeLayer1.push(
@@ -99,10 +104,10 @@ function initialize() {
       var JSTSpoly1 = geometryFactory.createPolygon(
         geometryFactory.createLinearRing(googleMaps2JSTS(poly1.getPath())));
       JSTSpoly1.normalize();
-      var JSTSpolyUnion = JSTSpoly1;//holeLayer1を和集合ポリゴンに代入
+      JSTSpolyUnion = JSTSpoly1;//holeLayer1を和集合ポリゴンに代入
       var outputPath = jsts2googleMaps(JSTSpolyUnion);
       var points = [kageLayer, outputPath];//pathを結合する
-      var x = new google.maps.Polygon({
+      x = new google.maps.Polygon({
         paths: points,
         strokeColor: '#808080',
         strokeOpacity: 0.9,
@@ -111,42 +116,7 @@ function initialize() {
         fillOpacity: 0.9
       });
       x.setMap(map);//mapにポリゴンを表示
-
-      var loop = function(){//holeLayer2を時間差で表示する
-        var holeLayer2 = [];//holeLayer2初期化
-        x.setMap(null);//古いポリゴンを除去
-        getPosition();
-        for (var i = 1; i < 65538; i++) {
-          holeLayer2.push(
-            {lat: ido + lat25 * Math.sin( angle * i * (Math.PI / 180) ) ,lng: keido + lng25 * Math.cos( angle * i * (Math.PI / 180) )}
-          );
-        }
-        holeLayer2.push(holeLayer2[0]);
-        poly2 = new google.maps.Polygon({
-          // map: map,
-          paths: [holeLayer2]
-        });
-        for (var i = 0; i < holeLayer2.length; i++) {
-          bounds.extend(new google.maps.LatLng(holeLayer2[i].lat, holeLayer2[i].lng));
-        }
-        JSTSpoly2 = geometryFactory.createPolygon(
-          geometryFactory.createLinearRing(googleMaps2JSTS(poly2.getPath())));
-        JSTSpoly2.normalize();
-        JSTSpolyUnion = JSTSpolyUnion.union(JSTSpoly2);//和集合を取る
-        outputPath = jsts2googleMaps(JSTSpolyUnion);
-        points = [kageLayer, outputPath];
-        x = new google.maps.Polygon({
-          paths: points,
-          strokeColor: '#808080',
-          strokeOpacity: 0.9,
-          strokeWeight: 2,
-          fillColor: '#808080',
-          fillOpacity: 0.9
-        });
-        //マップ上にポリゴンを表示
-        x.setMap(map);
-      }
-      setInterval(loop, 1000);//5000ミリ秒後に実行
+      setInterval(loop,5000);
     },
     // 取得失敗した場合
     function(error) {
@@ -164,6 +134,70 @@ function initialize() {
           alert("その他のエラー(エラーコード:"+error.code+")");
           break;
       }
+    },
+    {
+      enableHighAccuracy: true,
+    }
+  );
+}
+
+var loop = function(){//holeLayer2を時間差で表示する
+  var holeLayer2 = [];//holeLayer2初期化
+  navigator.geolocation.getCurrentPosition(
+    // 取得成功した場合
+    function(position) {
+      var ido2 = position.coords.latitude;
+      var keido2 = position.coords.longitude;
+      for (var i = 1; i < 65538; i++) {
+        holeLayer2.push(
+          {lat: ido2 + lat25 * Math.sin( angle * i * (Math.PI / 180) ) ,lng: keido2 + lng25 * Math.cos( angle * i * (Math.PI / 180) )}
+        );
+      }
+      holeLayer2.push(holeLayer2[0]);
+      var bounds = new google.maps.LatLngBounds();
+      poly2 = new google.maps.Polygon({
+        // map: map,
+        paths: [holeLayer2]
+      });
+      for (var i = 0; i < holeLayer2.length; i++) {
+        bounds.extend(new google.maps.LatLng(holeLayer2[i].lat, holeLayer2[i].lng));
+      }
+      var geometryFactory = new jsts.geom.GeometryFactory();
+      JSTSpoly2 = geometryFactory.createPolygon(
+        geometryFactory.createLinearRing(googleMaps2JSTS(poly2.getPath())));
+        JSTSpoly2.normalize();
+        JSTSpolyUnion = JSTSpolyUnion.union(JSTSpoly2);//和集合を取る
+        var outputPath = jsts2googleMaps(JSTSpolyUnion);
+        var points = [kageLayer, outputPath];
+      x.setMap(null);//古いポリゴンを除去
+      x = new google.maps.Polygon({
+        paths: points,
+        strokeColor: '#808080',
+        strokeOpacity: 0.9,
+        strokeWeight: 2,
+        fillColor: '#808080',
+        fillOpacity: 0.9
+      });
+      //マップ上にポリゴンを表示
+      x.setMap(map);
+      //setTimeout(loop, 5000);//5000ミリ秒後に実行
+    },
+    // 取得失敗した場合
+    function(error) {
+      switch(error.code) {
+        case 1: //PERMISSION_DENIED
+          alert("位置情報の利用が許可されていません");
+          break;
+        case 2: //POSITION_UNAVAILABLE
+          alert("現在位置が取得できませんでした");
+          break;
+        case 3: //TIMEOUT
+          alert("タイムアウトになりました");
+          break;
+        default:
+          alert("その他のエラー(エラーコード:"+error.code+")");
+          break;
+        }
     },
     {
       enableHighAccuracy: true,
