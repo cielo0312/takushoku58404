6dbbb5bc3f546d3ad3f210029aa733f98ba85801
owner <owner@ownernoMacBook-puro.local>
2018-07-26 14:26:43 +0900

    ver 1.1.1

--- Map2.js
+++ Map2.js
@@ -272,7 +272,7 @@ var loop = function(){//holeLayer2を時間差で表示する
         });
         //マップ上にポリゴンを表示
         x.setMap(map);
-        setTimeout(loop,5000);
+        setTimeout(loop,1000);
         //map.fitBounds(bounds);
         //setTimeout(loop, 5000);//5000ミリ秒後に実行
       //}else {
